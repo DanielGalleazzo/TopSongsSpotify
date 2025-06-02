c01137c76c365caaf510bcb209e0877656e86490
Daniel Augusto Galleazzo <danielgalleazzo@gmail.com>
2025-06-02 19:07:28 -0300

    Usuário pode escolher a quantia de músicas (5,10,25 e 50)

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
-const token = 'BQBbnMUFYKURHUih5-OkGHvIsWG4ntUNGsrFQSyZhTQOa4F8fhAJzVy0UUho2R_lrHmoBy65vr_faJMk6Tcr4G06UmE7s4iHKBSGZK6RlPRKPtcwWfRN5YhJAAYg-gVTWFs4iDk83xClk9OXKKCM3mD_8rOXTkVHfesISagl7-85ZQ2HcZamKNLKBgCWmqJziubMzJbvQM56MfPvbk29fPwhqm4tzk80CZCu0lk_rSRlKhwE6RzLTDBQfhkxYWh7DMFcWz0LOA13N3Xe_mNBqcxw73KxfLrF1PJ13YSn7dksyZ0sMpzrRxw';
+const token = 'BQCy7UQXJygeavtMQ7-q5o2flgK8yIfJdCVg6_z62MR3p-HhXGO_bwiAwEMyfy2f7JRbCVNTXIHsMc_0MdoF8az7CVrLJboVmq9CyBh4Bf5p2RRPXxZJc9wQWrNPS_8rpe0DRhRJyCw7qrF-Afmi4Xg1yM_5_Sl1m5cv0MezAlvtIlZqqjet6oQrwPyoUsynI9peZnysvDpBqAe2HsX_RZSIzlJUe3g0HBga2a1wCBrGWnV2FzScz6_D3gqe0RrUk3jrAD_r0XluzAII9UvOD5POZzGo_Ieo2cZmLErRVhABTQpXvQbn5G0';
 
-async function getTopTracks() {
+async function getTopTracks() { 
+  const res = await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50', {
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  return (await res.json()).items;
+}
+  async function getTopTracks5() {
   const res = await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=5', {
     headers: { Authorization: `Bearer ${token}` }
   });
   return (await res.json()).items;
 }
-document.getElementById('carregarMusicas').onclick = async () => {
-  const tracks = await getTopTracks();
+  async function getTopTracks10() {
+  const res = await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=10', {
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  return (await res.json()).items;
+}
+  async function getTopTracks25() {
+  const res = await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=25', {
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  return (await res.json()).items;
+}
+  async function getTopTracks50() {
+  const res = await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50', {
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  return (await res.json()).items;
+}
+
+document.getElementById('Quantidade5').onclick = async () => {
+  const tracks = await getTopTracks5();
+  document.getElementById('ListaDeMusicas').innerHTML = tracks.map(
+    t => `<li>${t.name} — ${t.artists.map(a => a.name).join(', ')}</li>`
+  ).join('');
+};
+
+document.getElementById('Quantidade10').onclick = async () => {
+  const tracks = await getTopTracks10();
+  document.getElementById('ListaDeMusicas').innerHTML = tracks.map(
+    t => `<li>${t.name} — ${t.artists.map(a => a.name).join(', ')}</li>`
+  ).join('');
+};
+
+document.getElementById('Quantidade25').onclick = async () => {
+  const tracks = await getTopTracks25();
+  document.getElementById('ListaDeMusicas').innerHTML = tracks.map(
+    t => `<li>${t.name} — ${t.artists.map(a => a.name).join(', ')}</li>`
+  ).join('');
+};
+document.getElementById('Quantidade50').onclick = async () => {
+  const tracks = await getTopTracks50();
   document.getElementById('ListaDeMusicas').innerHTML = tracks.map(
     t => `<li>${t.name} — ${t.artists.map(a => a.name).join(', ')}</li>`
   ).join('');
